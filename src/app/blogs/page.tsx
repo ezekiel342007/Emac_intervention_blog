148e35f175234097cffb74aae41c008e25d3b0a5
ezekiel342007 <ezekiel342007@gmail.com>
2025-08-16 14:20:58 +0100

    ...

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,22 +1,85 @@
-import BlogList from "@/components/function/blog-list";
+"use client";
+
 import Link from "next/link";
-import { JSX } from "react";
+import { JSX, useEffect, useState } from "react";
+import BlogCard from "@/components/function/blog-card";
+import { Post, PostResponse } from "@/types/Posts";
+import { Button } from "../../components/ui/button";
+
+async function getBlogs(url: string): Promise<PostResponse> {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error("failed to fetch");
+  }
+  return res.json();
+}
 
 export default function Page(): JSX.Element {
-  const url = process.env.NEXT_PUBLIC_PAGINATED_ALL_POSTS_ENDPOINT;
+  const [url, setUrl] = useState(process.env.NEXT_PUBLIC_PAGINATED_ALL_POSTS_ENDPOINT as string);
+  const [posts, setPosts] = useState<PostResponse | null>(null);
+  const [loading, setLoading] = useState(true);
+  const postEndpoint = process.env.NEXT_PUBLIC_ALL_POSTS_ENDPOINT;
+  const postLimit = process.env.NEXT_PUBLIC_POSTS_LIMIT;
+
+  useEffect(() => {
+    setLoading(true);
+    getBlogs(url)
+      .then((data) => setPosts(data))
+      .finally(() => setLoading(false));
+    console.log(url);
+  }, [url]);
+
+  const totalPages = posts?.results.length ? Math.ceil((posts.count ?? 0) / posts.results.length) : 0;
 
   return (
     <div className="m-12">
       <div className="mb-5 flex justify-between w-full">
         <Link href="/blogs/">All Posts</Link>
         <ul className="flex space-x-4 relative left-0">
-          <li><Link href="/blogs/trending/">Trending</Link></li>
-          <li><Link href="/blogs/read/">Read</Link></li>
-          <li><Link href="/blogs/unread/">Unread</Link></li>
-          <li><Link href="/blogs/latest/">Latest</Link></li>
+          <li><Button onClick={() => setUrl(process.env.NEXT_PUBLIC_LATEST_POSTS_ENDPOINT as string)}>Trending</Button></li>
+          <li><Button >Read</Button></li>
+          <li><Button >Unread</Button></li>
+          <li><Button onClick={() => setUrl(process.env.NEXT_PUBLIC_LATEST_POSTS_ENDPOINT as string)}>Latest</Button></li>
         </ul>
       </div>
-      <BlogList fetchUrl={`${url}`} />
+      <div>
+        {
+          (loading || !posts) ? <p>loading...</p> :
+            <div>
+              <div className="grid grid-cols-3 w-full">
+                {
+                  posts.results.map((post: Post): JSX.Element => {
+                    return <BlogCard key={post.id} postDetail={post} />
+                  })
+                }
+              </div>
+              <div className="flex justify-center">
+                <div>
+                  {
+                    <Button className="mr-2 z-10" disabled={posts.previous ? false : true} variant={posts.previous ? "default" : "ghost"} onClick={() => setUrl(posts.previous)}>Previous</Button>
+                  }
+                  {
+                    Array.from({ length: totalPages }).map((_, index) => {
+                      return (
+                        <Button
+                          key={index}
+                          className="w-fit h-fit rounded-4xl mr-1 z-10"
+                          onClick={
+                            () => setUrl(
+                              `${postEndpoint}?limit=${postLimit}&offset=${(index) * Number(postLimit)}`
+                            )
+                          }
+                        >{index + 1}</Button>)
+                    })
+                  }
+                  {
+                    <Button className="mr-2 z-10" disabled={posts.next ? false : true} variant={posts.next ? "default" : "ghost"} onClick={() => setUrl(posts.next)}>Next</Button>
+                  }
+                </div>
+              </div>
+            </div >
+        }
+      </div>
     </div >
   )
 }
--- src/app/blogs/read/page.tsx
+++
@@ -1,6 +0,0 @@
-import { JSX } from "react";
-
-export default function Page(): JSX.Element {
-
-  return <h2></h2>
-}
