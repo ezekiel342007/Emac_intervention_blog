da5b16b95e146d91afd233e61caa22d07ba0c26a
ezekiel342007 <ezekiel342007@gmail.com>
2025-11-11 10:26:38 +0100

    corrected use of useDetail

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-import { User } from "@/types/Posts";
+// import { User } from "@/types/Posts";
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -20,21 +20,21 @@ export const refreshToken = async (): Promise<void> => {
   }
 }
 
-export const userDetails = async (): Promise<User | undefined> => {
-  try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_CURRENT_USER_ENDPOINT}`);
-
-    if (response.status == 401 || response.status == 403) {
-      return undefined;
-    }
-
-    if (!response.ok)
-      throw new Error("UserDetailsError: ", await response.json())
-
-    return (await response.json());
-  } catch (error) {
-    console.error("Session verification failed", error);
-    return (undefined);
-  }
-}
+// export const userDetails = async (): Promise<User | undefined> => {
+//   try {
+//     const response = await fetch(`${process.env.NEXT_PUBLIC_CURRENT_USER_ENDPOINT}`);
+//
+//     if (response.status == 401 || response.status == 403) {
+//       return undefined;
+//     }
+//
+//     if (!response.ok)
+//       throw new Error("UserDetailsError: ", await response.json())
+//
+//     return (await response.json());
+//   } catch (error) {
+//     console.error("Session verification failed", error);
+//     return (undefined);
+//   }
+// }
 
