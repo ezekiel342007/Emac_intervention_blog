fb8b6539dc568178161ad2a804db0b5c89f18334
ezekiel342007 <ezekiel342007@gmail.com>
2025-11-11 11:25:40 +0100

    adjusted register page again

--- src/components/function/register-form.tsx
+++ src/components/function/register-form.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
